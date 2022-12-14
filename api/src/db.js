e8d3bc91d2bb7a8e80d4ef715c054d857e036323
Santiago3896 <santigini96@gmail.com>
2022-12-14 20:26:26 -0300

    e

--- api/src/db.js
+++ api/src/db.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT
-} = process.env.PORT;
+} = process.env;
 
 let sequelize =
-  process.env.PORT.NODE_ENV === "production"
+  process.env.NODE_ENV === "production"
     ? new Sequelize({
         database: DB_NAME,
         dialect: "postgres",
