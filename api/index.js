8f13d064ed731ac694e33b984df57da850f90e60
Santiago3896 <santigini96@gmail.com>
2022-12-14 20:54:14 -0300

    a

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => { // SI LO QUIERO VER EN LOCALHOST CAMBIO TODO POR EL 3001//
+  server.listen(PORT, () => { // SI LO QUIERO VER EN LOCALHOST CAMBIO TODO POR EL 3001//
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
